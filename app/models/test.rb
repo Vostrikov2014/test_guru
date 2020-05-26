b9f634b260069cf01a584d334b847b6893cedd47
vostrikov2014 <vostrikov2014@gmail.com>
2020-05-26 22:42:22 +0300

    lesson_4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,9 @@
 class Test < ApplicationRecord
+
+  belongs_to :category
+  scope :sort_category, ->(category) { joins(:category).where(categories: { title: category }) }
+
+  def self.sort_by_category(category_name)
+    sort_category(category_name).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  def user_tests_list(level)
+    self.tests.where(level: level)
+  end
 end
