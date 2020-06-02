fea283c5cf83a2e158a1477c953cef295ac1d691
vostrikov2014 <vostrikov2014@gmail.com>
2020-06-02 18:42:44 +0300

    lesson_5

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :results
   has_many :users, through: :results
+  belongs_to :author, class_name: 'User'
 
   def self.sort_by_category(name)
     joins(:category).where(categories: { title: name }).order(title: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
 
   has_many :results
   has_many :tests, through: :results
+  has_many :tests_author, class_name: 'Test', foreign_key: :author_id
 
   def user_results(level)
-    Test.joins(results: :user).where(level: level)
-    #tests.where(level: level)
+    #Test.joins(results: :user).where(level: level)
+    tests.where(level: level)
   end
 end
