3c4ded06d0544b6ba8cfa80ac5ee456dddd9d607
vostrikov2014  <vostrikov2014@gmail.com>
2020-07-04 17:05:03 +0300

    unit_10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_175949) do
+ActiveRecord::Schema.define(version: 2020_07_02_172917) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -61,9 +61,10 @@ ActiveRecord::Schema.define(version: 2020_06_22_175949) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
