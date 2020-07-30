fd5abd491f6cbc8384c624b202e94f9438871a42
vostrikov2014  <vostrikov2014@gmail.com>
2020-07-30 09:43:17 +0300

    unit_17

--- db/migrate/20200724122626_add_indexes.rb
+++
@@ -1,7 +0,0 @@
-#class AddIndexes < ActiveRecord::Migration[6.0]
-#  def change
-#    add_index :tests, :author_id
-#    add_index :tests, :category_id
-#    add_index :tests, %i[title level], unique: true
-#  end
-#end
