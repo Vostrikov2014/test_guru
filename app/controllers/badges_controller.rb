87beb6db9eb05a024d7869effb4107c5352bc653
vostrikov2014  <vostrikov2014@gmail.com>
2020-08-07 19:42:13 +0300

    unit_18_1_1

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,6 +1,8 @@
 class BadgesController < ApplicationController
+
   def index
+    #@user_badges = current_user.badges
+    @user_badges = Badge.all
     @badges = Badge.all
-    @user_badges = current_user.badges
   end
 end
